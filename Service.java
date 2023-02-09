e2f6e349bfafcaf9b19a44f077be9d2edc950309
vinodkadam02 <vinod.kadam@theelixrlabs.com>
2023-02-09 12:00:10 +0530

    commit all

--- Service.java
+++ Service.java
@@ -2,5 +2,6 @@ public class Service {
     public static void main(String[] args) {
         System.out.println("This is Service Class");
         System.out.println("Welcome to Service");
+        System.out.println("");
     }
 }
