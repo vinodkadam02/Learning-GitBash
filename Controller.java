fd3e63c9b56d8f890481fed1e78ee732845c57cf
vinodkadam02 <vinod.kadam@theelixrlabs.com>
2023-02-09 11:57:12 +0530

    Initial commit

---
+++ Controller.java
@@ -0,0 +1,6 @@
+public class Controller {
+    public static void main(String[] args) {
+        System.out.println("This is Controller Class");
+        System.out.println("Welcome to Controller");
+    }
+}
---
+++ Service.java
@@ -0,0 +1,6 @@
+public class Service {
+    public static void main(String[] args) {
+        System.out.println("This is Service Class");
+        System.out.println("Welcome to Service");
+    }
+}
